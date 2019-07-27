48ddd2b3cb66a3d75cd9c5f95bc8c29d36247cb4
Devon Bacon <devon.bacon@gmail.com>
2019-07-26 19:49:20 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <!-- <img width="25%" src="./assets/logo.png">
+    <HelloWorld msg="Hello Vue in CodeSandbox!" />-->
+    <div class="draggable box" v-on:click="drag"></div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  },
+  methods: {
+    drag: function(event) {
+      console.log(event);
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.box {
+  background: green;
+  height: 50px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 50px;
+}
+</style>
